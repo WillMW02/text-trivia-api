5a635e5a5846673b6f2c797547dd86e36f42a036
WillMW02 <williammw.wmw@gmail.com>
2022-02-19 22:38:38 +0000

    feat: add first database query structure

---
+++ src/models/question.model.js
@@ -0,0 +1,19 @@
+import * as PgSQL from '../lib/pgsql.js';
+import sqlCommands from '../config/sqlCommands.json';
+import logger from '../lib/logger.js';
+
+export const get = async (id = null) => {
+	const client = await PgSQL.connect();
+	try {
+		const res = await client.query(
+			id?sqlCommands.questions.getQuestion:sqlCommands.questions.getLatestQuestion, 
+			id?[id]:null
+		);
+		return res.rows[0];
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occured whilst fetching a question');
+	} finally {
+		if(client) client.release();
+	}
+};
