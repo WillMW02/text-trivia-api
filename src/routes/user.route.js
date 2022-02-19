afa584d00cf9143eb5c053a0e09ae9666f96b3d3
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-02-19 22:24:24 +0000

    fix: adjusted the controller routes as necessary

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,8 +3,16 @@ import * as UserController from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.post('/', UserController.getUsers);
+// TODO add JWT middleware
+router.get('/me', UserController.getOwnUser);
 
+// ? TODO: this feature might be useful later
 router.get('/:id', UserController.getUser);
+
+// TODO add JWT & CSRF middleware
+router.post('/create', UserController.createUser);
+
+// TODO add JWT & CSRF middleware
+router.post('/set-phone', UserController.setUserPhone);
 
 export default router;
