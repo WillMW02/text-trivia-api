e67ac51b68586fa0b250c4b2a4d8009bedd53751
WillMW02 <williammw.wmw@gmail.com>
2022-02-19 15:18:33 +0000

    feat: add login endpoint

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import UserRouter from './user.route.js';
+import LoginRouter from './login.route.js';
+import app from '../app.js';
 
 const router = Router();
 
 app.use('/user/', UserRouter);
+app.use('/login/', LoginRouter);
 
 export default router;
---
+++ src/routes/login.route.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as LoginController from '../controllers/login.controller.js';
+
+const router = Router();
+
+// router.get('/example', LoginController.method);
+
+export default router;
