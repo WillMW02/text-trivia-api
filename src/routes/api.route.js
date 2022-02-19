ddc006e34917da8252ff983804800bd61fd7617c
WillMW02 <williammw.wmw@gmail.com>
2022-02-19 15:25:42 +0000

    feat: add score endpoint

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import UserRouter from './user.route.js';
 import LoginRouter from './login.route.js';
+import ScoreRouter from './score.route.js';
 import app from '../app.js';
 
 const router = Router();
 
 app.use('/user/', UserRouter);
 app.use('/login/', LoginRouter);
+app.use('/score/', ScoreRouter);
 
 export default router;
---
+++ src/routes/score.route.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as ScoreController from '../controllers/score.controller.js';
+
+const router = Router();
+
+// router.get('/example', ScoreController.method);
+
+export default router;
