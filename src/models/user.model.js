9604696683189720716a52e8eacf9f5658c770da
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-02-19 23:38:05 +0000

    fix: hash password with bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,7 @@
-const bcrypt = require("bcrypt");
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
+import { hash } from "bcrypt";
 
-const hashPassword = (password) => {
-    bcrypt.hash(myPlaintextPassword, function(err, hash) {
-        // Store hash in your password DB.
-    });
+const salt_rounds = 10;
+
+const hashPassword = async (password) => {
+    return await hash(password, salt_rounds);
 }
