727c713280ff78909b8c57fe75258ae219883b91
blazerage32 <40123555+blazerage32@users.noreply.github.com>
2022-02-19 15:32:16 +0000

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,14 @@ import * as UserController from '../controllers/user.controller.js';
 
 const router = Router();
 
+router.get("/", (req, res, next) => {
+
+})
+
+router.get("/:id", (req, res, next) => {
+    const id = req.params.id
+    // Code here finds the details of the user with the id sent through the parameter.
+})
 // router.get('/example', UserController.method);
 
 export default router;
