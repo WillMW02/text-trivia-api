7033eb3f3399365d34909b70d1021564021c53ca
blazerage32 <40123555+blazerage32@users.noreply.github.com>
2022-02-19 17:23:28 +0000

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,10 @@
 export const getUsers = (req, res, next) => {
+
 }
+
+
 export const getUser = (req, res, next) => {
-
     const id = req.params.id
-// Code here finds the details of the user with the id sent through the parameter.
-
+    // Code here finds the details of the user with the id sent through the parameter.
 }
 
