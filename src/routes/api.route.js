ee6cf5adf2ee1acce60c07cebe8efb5516b9a558
WillMW02 <williammw.wmw@gmail.com>
2022-02-19 15:29:41 +0000

    feat: add question endpoint

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserRouter from './user.route.js';
 import LoginRouter from './login.route.js';
 import ScoreRouter from './score.route.js';
+import QuestionRouter from './question.route.js';
 import app from '../app.js';
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 app.use('/user/', UserRouter);
 app.use('/login/', LoginRouter);
 app.use('/score/', ScoreRouter);
+app.use('/question/', QuestionRouter);
 
 export default router;
---
+++ src/routes/question.route.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as QuestionController from '../controllers/question.controller.js';
+
+const router = Router();
+
+// router.get('/example', QuestionController.method);
+
+export default router;
