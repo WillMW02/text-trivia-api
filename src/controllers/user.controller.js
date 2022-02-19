1fad8c7cdf3f9988ce4a5d31838118ac4756d53f
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-02-19 23:50:58 +0000

    style: go away will

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,16 @@
 export const getUser = (req, res, next) => {
 	const id = req.params.id;
 	// Code here finds the details of the user with the id sent through the parameter.
-}
+};
 
 export const getOwnUser = (req, res, next) => {
 	// user id is held in JWT
-}
+};
 
 export const createUser = (req, res, next) => {
 
-}
+};
 
 export const setUserPhone = (req, res, next) => {
 	
-}
+};
