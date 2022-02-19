da8e306b8bfd846ec302c56717520ac39693cd9f
WillMW02 <williammw.wmw@gmail.com>
2022-02-19 17:50:24 +0000

    chore: remove redundant code from users route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,14 +3,8 @@ import * as UserController from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.post("/", UserController.getUser)
+router.post('/', UserController.getUsers);
 
-})
-
-router.get("/:id", (req, res, next) => {
-    const id = req.params.id
-    // Code here finds the details of the user with the id sent through the parameter.
-})
-// router.get('/example', UserController.method);
+router.get('/:id', UserController.getUser);
 
 export default router;
