68c107a18528c59da126fe2dc1bc40325de80c66
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 02:01:18 +0000

    fix: correct user get method

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,7 @@
-import * as PgSQL from "../lib/pgsql";
-import sqlCommands from "../config/sqlCommands.json";
-import logger from "../lib/logger";
-
-const bcrypt = require("bcrypt");
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
+import * as PgSQL from '../lib/pgsql';
+import sqlCommands from '../config/sqlCommands.json';
+import logger from '../lib/logger';
+import bcrypt from 'bcrypt';
 
 const hashPassword = (password) => {
     bcrypt.hash(myPlaintextPassword, function(err, hash) {
@@ -14,19 +12,17 @@ const hashPassword = (password) => {
 export const get = async (id = null) => {
     const client = await PgSQL.connect();
     try {
-        const res = await client.query(
-            id?sqlCommands.users.getUser:sqlCommands.users.getPassword,
-            id?[id]:null
-        );
+        const res = await client.query(sqlCommands.users.getUser, [id]);
         return res.rows[0];
     } catch(err) {
         logger.error(err, true);
-        throw new Error('An error occurred whilst fetching a question');
+        throw new Error('An error occurred whilst fetching a user');
     } finally {
         if(client) client.release();
     }
 };
-import { hash } from "bcrypt";
+
+import { hash } from 'bcrypt';
 
 const salt_rounds = 10;
 
