3c706b51ec80a9b1c9e75525af32e83091b44fb8
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 05:43:11 +0000

    feat: add functionality to get own user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,20 @@
-export const getUser = (req, res, next) => {
-	const id = req.params.id;
-	// Code here finds the details of the user with the id sent through the parameter.
-};
+import logger from '../lib/logger.js';
+import * as UserModel from '../models/user.model.js';
 
-export const getOwnUser = (req, res, next) => {
-	// user id is held in JWT
+export const getOwnUser = async (req, res, next) => {
+	const id = req.user.id;
+	try {
+		if(!id) throw new Error('No user ID could be found');
+		const dat = await UserModel.get(id);
+		if(dat) res.json(dat);
+		res.status(404);
+		res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
 };
 
 export const createUser = (req, res, next) => {
