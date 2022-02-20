d8d7031ca1b4132d5728e6b32f8a5332f22978c4
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 03:41:05 +0000

    feat: add user creation to model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,13 +4,28 @@ import logger from '../lib/logger';
 import { hash } from 'bcrypt';
 
 export const get = async (id = null) => {
-	const client = await PgSQL.connect();
+	let client;
 	try {
+		client = await PgSQL.connect();
 		const res = await client.query(sqlCommands.users.getUser, [id]);
 		return res.rows[0];
 	} catch(err) {
 		logger.error(err, true);
 		throw new Error('An error occurred whilst fetching a user');
+	} finally {
+		if(client) client.release();
+	}
+};
+
+export const create = async (username, pass_hash, mobile_no) => {
+	let client;
+	try {
+		client = await PgSQL.connect();
+		const res = await client.query(sqlCommands.users.createUser, [username, pass_hash, mobile_no]);
+		return res.rows[0];
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occurred whilst creating a user');
 	} finally {
 		if(client) client.release();
 	}
