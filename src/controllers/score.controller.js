7f2786f994b49aa816a08186cde94390f7825978
blazerage32 <40123555+blazerage32@users.noreply.github.com>
2022-02-20 04:32:57 +0000

    Update score.controller.js

--- src/controllers/score.controller.js
+++ src/controllers/score.controller.js
@@ -16,7 +16,22 @@ export const getScores = async (req, res, next) => {
 
 };
 
-export const getScore = (req, res, next) => {
-	id = req.params.id;
-	const score = ScoreModel.get(id);
+export const getScore = async (req, res, next) => {
+	let id = req.params.id;
+	if (id == 'me') {
+		//TODO: Add code to get the current user ID
+	}
+
+	try {
+		const score = await ScoreModel.get(id);
+		if(score) res.json(score);
+		res.status(404);
+		res.send();
+	} catch (e) {
+		res.status(500);
+		res.send();
+		next(e);
+		return;
+	}
+
 };
