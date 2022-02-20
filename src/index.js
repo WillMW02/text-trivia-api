07ef8ae453cd7ba6f63c2e21faaac6bf9fac3449
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 08:41:27 +0000

    chore: move Questionhost and add invocation point

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import './config/env.config.js';
 import server from './app.js';
 import logger from './lib/logger.js';
-import QuestionHost from './lib/questionHost.js';
 
 server.listen(process.env.PORT??8080, () => {
 	logger.info(`Listening on port ${process.env.PORT??8080}`);
 });
-
-const questionHost = new QuestionHost();
-questionHost.scheduleCron();
-// the troubled one
-QuestionHost.instance = questionHost;
