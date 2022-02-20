c7b0320177ee915005100d7bd4036ee5c7cf2026
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-02-20 01:58:53 +0000

    feat: question host framework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './config/env.config.js';
 import server from './app.js';
 import logger from './lib/logger.js';
+import QuestionHost from './lib/questionHost.js';
 
 server.listen(process.env.PORT??8080, () => {
 	logger.info(`Listening on port ${process.env.PORT??8080}`);
 });
+
+const questionHost = new QuestionHost();
+questionHost.scheduleCron();
---
+++ src/lib/questionHost.js
@@ -0,0 +1,41 @@
+import nodeCron from 'node-cron';
+
+export default class QuestionHost {
+	constructor() {
+	}
+
+	scheduleCron() {
+		const hour = Math.floor(Math.random() * 24);
+
+		if (this.cron) {
+			this.unscheduleCron();
+		}
+
+		this.cron = nodeCron.schedule(`0 ${hour} * * *`, () => {
+			console.debug('Triggering scheduled job now');
+			this.triggerNow(true);
+		});
+	}
+
+	unscheduleCron() {
+		if (this.cron) {
+			this.cron.stop();
+			this.cron = undefined;
+		}
+	}
+
+	async triggerNow(reloadQuestion = false) {
+		if (reloadQuestion) {
+			await this.loadQuestion();
+		}
+		if (this.currentQuestion) {
+			// TODO send the question to the peeps
+			return true;
+		}
+		return false;
+	}
+
+	async loadQuestion() {
+		// TODO load a question from the DB
+	}
+}
