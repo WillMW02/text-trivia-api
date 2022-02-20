32ff8c91f34f4028b4937b4f8ae6057a17696ada
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 02:08:19 +0000

    feat: add BCrypt salting rounds to environment variables

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,31 +1,21 @@
 import * as PgSQL from '../lib/pgsql';
 import sqlCommands from '../config/sqlCommands.json';
 import logger from '../lib/logger';
-import bcrypt from 'bcrypt';
-
-const hashPassword = (password) => {
-    bcrypt.hash(myPlaintextPassword, function(err, hash) {
-        // Store hash in your password DB.
-    });
-}
-
-export const get = async (id = null) => {
-    const client = await PgSQL.connect();
-    try {
-        const res = await client.query(sqlCommands.users.getUser, [id]);
-        return res.rows[0];
-    } catch(err) {
-        logger.error(err, true);
-        throw new Error('An error occurred whilst fetching a user');
-    } finally {
-        if(client) client.release();
-    }
-};
-
 import { hash } from 'bcrypt';
 
-const salt_rounds = 10;
+export const get = async (id = null) => {
+	const client = await PgSQL.connect();
+	try {
+		const res = await client.query(sqlCommands.users.getUser, [id]);
+		return res.rows[0];
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occurred whilst fetching a user');
+	} finally {
+		if(client) client.release();
+	}
+};
 
 const hashPassword = async (password) => {
-    return await hash(password, salt_rounds);
-}
+	return await hash(password, process.env.BCRYPT_SALT_ROUNDS);
+};
