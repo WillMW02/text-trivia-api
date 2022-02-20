116f1ac02ef52463a0ce63774141836933fb34b3
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 10:24:48 +0000

    chore: debug statements

--- src/lib/bridgeSource.js
+++ src/lib/bridgeSource.js
@@ -1,15 +1,17 @@
 import fetch from 'node-fetch';
+import logger from '../lib/logger.js';
 
 const bridgeUrl = process.env.BRIDGE_SOURCE_URL;
 const psk = process.env.HACKY_PSK;
 
 export async function sendTwilioMessage(message, to) {
+	logger.info(JSON.stringify({ message, to }), true);
 	const response = await fetch(`${bridgeUrl}send`, {
 		method: 'POST',
 		headers: {
-			Accept: 'application/json',
+			'Accept': 'application/json',
 			'Content-Type': 'application/json',
-			psk: psk
+			'psk': psk
 		},
 		body: JSON.stringify({ message, to }),
 	});
