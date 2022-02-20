aecc96761f426d88749a4e3f6d1b91350ff92cb6
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 11:54:47 +0000

    fix: fix endpoint

--- src/controllers/bridge.controller.js
+++ src/controllers/bridge.controller.js
@@ -2,35 +2,30 @@ const stopStr = process.env.STOP_STRING;
 const frontend = process.env.FRONTEND_URL;
 
 export const getResponseHandler = (questionHost) => {
-	return (req, res) => {
+	return async (req, res) => {
 		const number = req.body.number;
 		const messageResponse = req.body.response.toLowerCase();
 
 		if (messageResponse == stopStr) {
-			if (questionHost.endCommunications(number)) {
-				res.json({ message: `Thank you for participating! Visit ${frontend} if you wish to sign up again!` });
+			if (await questionHost.endCommunications(number)) {
+				return res.json({ message: `Thank you for participating! Visit ${frontend} if you wish to sign up again!` });
 			} else {
-				res.json({ message: `Visit ${frontend} to sign up to TextTrivia` });
+				return res.json({ message: `Visit ${frontend} to sign up to TextTrivia` });
 			}
 		} else {
-			const successState = questionHost.check(number, messageResponse);
+			const successState = await questionHost.check(number, messageResponse);
 			switch (successState) {
 			case 1:
-				res.json({ message: 'Congratulations!' });
-				break;
+				return res.json({ message: 'Congratulations!' });
 			case 0:
-				res.json({ message: 'Incorrect answer!' });
-				break;
+				return res.json({ message: 'Incorrect answer!' });
 			case -1:
-				res.json({ message: `You have already answered this question! If you wish to stop receiving messages, type "${stopStr}"` });
-				break;
+				return res.json({ message: `You have already answered this question! If you wish to stop receiving messages, type "${stopStr}"` });
 			case -2:
-				res.json({ message: `Today's question is not available yet! If you wish to stop receiving messages, type "${stopStr}"` });
-				break;
+				return res.json({ message: `Today's question is not available yet! If you wish to stop receiving messages, type "${stopStr}"` });
 			default:
 				// TODO laziness means I just copied this line from above
-				res.json({ message: `Visit ${frontend} to sign up to TextTrivia` });
-				break;
+				return res.json({ message: `Visit ${frontend} to sign up to TextTrivia` });
 			}
 		}
 	};
