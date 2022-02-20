4186a6e37d3ce77a4c4b20ee21f54ab678a076be
blazerage32 <40123555+blazerage32@users.noreply.github.com>
2022-02-20 04:07:35 +0000

    Update score.controller.js

--- src/controllers/score.controller.js
+++ src/controllers/score.controller.js
@@ -1,9 +1,22 @@
 import * as ScoreModel from '../models/score.models.js';
 
-export const getScores = (req, res, next) => {
+export const getScores = async (req, res, next) => {
+
+	try {
+		const scores = await ScoreModel.getMultiple();
+		if(scores) res.json(scores);
+		res.status(404);
+		res.send();
+	} catch(e) {
+		res.status(500);
+		res.send();
+		next(e);
+		return;
+	}
 
 };
 
 export const getScore = (req, res, next) => {
-
+	id = req.params.id;
+	const score = ScoreModel.get(id);
 };
