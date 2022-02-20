e159e46cbfd6d86a7eec7df983670c935cd937dc
WillMW02 <williammw.wmw@gmail.com>
2022-02-20 06:23:20 +0000

    feat: add user creation functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import logger from '../lib/logger.js';
+import { hashPassword } from '../lib/auth.js';
 import * as UserModel from '../models/user.model.js';
 
 export const getOwnUser = async (req, res, next) => {
@@ -17,8 +18,28 @@ export const getOwnUser = async (req, res, next) => {
 	}
 };
 
-export const createUser = (req, res, next) => {
+export const createUser = async (req, res, next) => {
+	if(!(req.body && req.body.username && req.body.password && req.body.mobile_no)) {
+		res.status(406);
+		if(!(req.body.mobile_no && /^([+]\d{2})?\d{10}$/.test(req.body.mobile_no.replace(/\s/g, '')))) return res.json({
+			err: 'Request body did not include a valid phone number'
+		}); 
+		return res.json({
+			err: 'Request body did not include required parameters'
+		});
+	};
 
+	const pass_hash = await hashPassword(req.body.password);
+
+	try {
+		await UserModel.create(req.body.username, pass_hash, req.body.mobile_no);
+		res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
 };
 
 export const setUserPhone = (req, res, next) => {
